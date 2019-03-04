5a5e772f6cf791154d1ad2efe5943382e154919e
Valerio <valerioly@gmail.com>
2019-03-04 14:28:44 +0100

    get profile

--- app/src/main/java/com/stockato/ginzo/profileActivity.java
+++ app/src/main/java/com/stockato/ginzo/profileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,57 +14,74 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class profileActivity extends AppCompatActivity {
-TextView email;
-private DatabaseReference mDatabase;
+    TextView email;
+    Button logoutBtn;
+    private DatabaseReference mDatabase;
 
-Button logoutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-      getSupportActionBar().hide();
-      email = findViewById(R.id.email);
+        getSupportActionBar().hide();
 
-      mDatabase = FirebaseDatabase.getInstance().getReference();
-      FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
-      logoutBtn = findViewById(R.id.logoutBtn);
 
+        email = findViewById(R.id.email);
+        logoutBtn = findViewById(R.id.logoutBtn);
+
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("User");
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            String email = user.getEmail();
-            String uid = user.getUid();
-        }
+
+
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+
+                    for (DataSnapshot chidSnap : dataSnapshot.getChildren()) {
+                        Log.v("profile",""+ chidSnap.getKey());
+                        Log.v("profile",""+ chidSnap.child("email").getValue());
+                     email.setText((CharSequence) chidSnap.child("email").getValue());
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+                Log.w("profile", "Failed to read value.", error.toException());
+            }
+        });
 
 
 
 
 
 
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-
-
-      logoutBtn.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-
-              FirebaseAuth.getInstance().signOut();
-              if (user != null){
-                  Intent intent = new Intent(profileActivity.this, RegisterActivity.class);
-                  startActivity(intent);
-              }
-
-
-               else {
-              Toast.makeText(profileActivity.this, "Logout Fallito", Toast.LENGTH_SHORT).show();
-              }
-          }
-      });
-
+                FirebaseAuth.getInstance().signOut();
+                if (user != null) {
+                    Intent intent = new Intent(profileActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(profileActivity.this, "Logout Fallito", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
     }
