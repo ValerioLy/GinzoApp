ad9de4d7a0b810e1eba169b10af577fdfef306b0
Valerio <valerioly@gmail.com>
2019-03-08 17:09:48 +0100

    PrenotaActivity

---
+++ app/src/main/java/com/stockato/ginzo/OreAdapter.java
@@ -0,0 +1,4 @@
+package com.stockato.ginzo;
+
+public class OreAdapter {
+}
---
+++ app/src/main/java/com/stockato/ginzo/OreTavolo.java
@@ -0,0 +1,13 @@
+package com.stockato.ginzo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OreTavolo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ore_tavolo);
+    }
+}
