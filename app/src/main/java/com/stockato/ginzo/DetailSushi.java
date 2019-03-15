d70f1640bef19bfe131daf9e7899bd876a8d00d2
Valerio <valerioly@gmail.com>
2019-03-15 15:59:17 +0100

    carrello, contatore detailsushi

--- app/src/main/java/com/stockato/ginzo/DetailSushi.java
+++ app/src/main/java/com/stockato/ginzo/DetailSushi.java
@@ -2,14 +2,21 @@ package com.stockato.ginzo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailSushi extends AppCompatActivity {
-    TextView titolotxt, descrizionetxt, prezzotxt;
+    TextView titolotxt, descrizionetxt, prezzotxt, numero, textShop;
     ImageView imgSushi;
+    ImageButton imgShop;
     String titolo, descrizione, prezzo;
     int img;
+    Button piu, meno, carrello;
+    int count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +29,16 @@ public class DetailSushi extends AppCompatActivity {
         prezzotxt = findViewById(R.id.prezzo);
         imgSushi = findViewById(R.id.imgSushi);
 
+        piu = findViewById(R.id.piu);
+        meno = findViewById(R.id.meno);
+        carrello = findViewById(R.id.btnCarrello);
+        numero = findViewById(R.id.numero);
 
-        if (getIntent().getExtras()!=null){
+        textShop = findViewById(R.id.textShop);
+        imgShop = findViewById(R.id.imgShop);
+
+
+        if (getIntent().getExtras() != null) {
             Bundle bundle = getIntent().getExtras();
             titolo = bundle.getString("titolo");
             descrizione = bundle.getString("descrizione");
@@ -37,7 +52,31 @@ public class DetailSushi extends AppCompatActivity {
         imgSushi.setImageResource(img);
 
 
+        meno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(count == 0){
+                }else {
+                    count--;
+                    numero.setText(String.valueOf(count));
+                }
+            }
+        });
 
+        piu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                numero.setText(String.valueOf(count));
+            }
+        });
+
+        carrello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textShop.setText(String.valueOf(count));
+            }
+        });
 
 
 
