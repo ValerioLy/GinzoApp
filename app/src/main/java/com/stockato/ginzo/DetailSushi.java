bb4a84ba33028c165f47f80867add244d18c0cd7
Valerio <valerioly@gmail.com>
2019-03-21 17:39:36 +0100

    numero carrello

--- app/src/main/java/com/stockato/ginzo/DetailSushi.java
+++ app/src/main/java/com/stockato/ginzo/DetailSushi.java
@@ -17,8 +17,11 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -33,10 +36,14 @@ public class DetailSushi extends AppCompatActivity {
     String titolo, descrizione, prezzo;
     int id, img;
     Button piu, meno, carrello;
-    int count;
+    int count, count2, count3;
     DatabaseReference reference;
     String key;
     ItemSushi itemSushi;
+    String idUser;
+    boolean check = false;
+    boolean premuto = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,6 +85,48 @@ public class DetailSushi extends AppCompatActivity {
         imgSushi.setImageResource(img);
 
 
+        final FirebaseUser utente = FirebaseAuth.getInstance().getCurrentUser();
+        idUser = utente.getUid();
+        reference = FirebaseDatabase.getInstance().getReference().child("Ordine");
+
+//////////////////
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot chidSnap : dataSnapshot.getChildren()) {
+                        String idUserChild = (String) chidSnap.child("idUser").getValue();
+
+                        if (check == false) {
+                            if (idUser.equals(idUserChild)) {
+                                count2 = count2 + 1;
+                                textShop.setText(String.valueOf(count2));
+
+                            }
+                        }
+
+
+                    }
+                    check = true;
+                    if (premuto == true ){
+                        textShop.setText(String.valueOf(count3));
+                    }
+
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+                Log.w("profile", "Failed to read value.", error.toException());
+            }
+        });
+
+/////////////////
+
+
         meno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -100,14 +149,14 @@ public class DetailSushi extends AppCompatActivity {
         carrello.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textShop.setText(String.valueOf(count));
                 key = reference.push().getKey();
                 itemSushi.setTitolo(titolotxt.getText().toString());
                 itemSushi.setPrezzo(prezzotxt.getText().toString());
                 itemSushi.setNumero(String.valueOf(count));
                 itemSushi.setIdUser(user.getUid());
-
                 reference.child(key).setValue(itemSushi);
+                count3 = count2 + 1;
+                premuto = true;
 
             }
         });
